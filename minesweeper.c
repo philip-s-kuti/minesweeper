ea1894c07c06a02481c8b2a5db3c9505c95d5840
U-NORITIKATINO\Phil's PC <v3ta6@unb.ca>
2025-06-30 10:59:53 -0300

    copied in-progress minesweeper file to new github repository

---
+++ minesweeper.c
@@ -0,0 +1,62 @@
+//p kuti
+//created file 30 june 2025
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//TODO
+/* the whole calculateCellMineValue
+ * implement termios
+ * implement the terminal clear screen character '\e[1;1H\e[2J'
+*/
+
+typedef struct Cell Cell;
+struct Cell {
+	bool isMine; //whether the cell contains a mine or not
+	int value; //how many mines are around the cell
+};
+
+void initCell (Cell* c, bool isMine) {
+	c->isMine = isMine;
+	c->value = 0;
+}
+
+//TODO
+void calculateCellMineValue(Cell* c) {
+	//TODO look at the 8 cells around every mine cell
+	//and increment them all by 1
+	//make sure to check for edge cases. literally in this case (only have to increment 5 cells when a mine is against the side of the board)
+}
+
+Cell** initBoard(int boardSize) { //since the board will always be square, we just take one value and square it for the dimensions of the board
+	
+	Cell** c = malloc(sizeof(Cell) * boardSize);
+	for(int i = 0; i < boardSize; i++) {
+		c[i] = malloc(sizeof(Cell) * boardSize);
+	}
+	return c;
+}
+
+void printBoard(Cell** board, int size) {
+	
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j < size; j++) {
+			printf("%d", board[i][j].value);
+		}
+		printf("\n");
+	}
+	
+}
+
+
+int main (int argc, char** argv) {
+	
+	int boardSize = 5;
+	Cell** board = initBoard(boardSize);
+	printBoard(board, boardSize);
+	
+	
+	
+	return EXIT_SUCCESS;
+}
