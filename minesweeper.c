81b99a15f885111905e140a5d6598459f752dc16
U-NORITIKATINO\Phil's PC <v3ta6@unb.ca>
2025-06-30 11:52:56 -0300

    added printBoard function

--- minesweeper.c
+++ minesweeper.c
@@ -31,18 +31,40 @@ void calculateCellMineValue(Cell* c) {
 
 Cell** initBoard(int boardSize) { //since the board will always be square, we just take one value and square it for the dimensions of the board
 	
+	//malloc the board
 	Cell** c = malloc(sizeof(Cell) * boardSize);
 	for(int i = 0; i < boardSize; i++) {
 		c[i] = malloc(sizeof(Cell) * boardSize);
 	}
+	
+	//set the initial values
+	for(int i = 0; i < boardSize; i++) {
+		for(int j = 0; j < boardSize; j++) {
+			c[i][j].value = 0;
+			c[i][j].isMine = false;
+		}
+	}
 	return c;
 }
+
 
 void printBoard(Cell** board, int size) {
 	
 	for(int i = 0; i < size; i++) {
 		for(int j = 0; j < size; j++) {
-			printf("%d", board[i][j].value);
+				
+			if(board[i][j].isMine) {
+				printf("O");
+			}
+			else {
+				if(board[i][j].value > 0) {
+					printf("%d", board[i][j].value);
+				}
+				else {
+					printf(".");
+				}
+			}
+			printf(" "); //for spacing
 		}
 		printf("\n");
 	}
@@ -52,7 +74,7 @@ void printBoard(Cell** board, int size) {
 
 int main (int argc, char** argv) {
 	
-	int boardSize = 5;
+	int boardSize = 20;
 	Cell** board = initBoard(boardSize);
 	printBoard(board, boardSize);
 	
